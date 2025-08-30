962bbbe624643357fdfb12a8554287a9f61a5231
Lino Khan <linokhan1@gmail.com>
2025-08-30 14:40:48 +0200

    updated the import for auth options in panic api routes

--- src/app/api/panic/history/route.ts
+++ src/app/api/panic/history/route.ts
@@ -1,7 +1,7 @@
 // src/app/api/panic/history/route.ts
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../auth/[...nextauth]/route";
+import { authOptions } from "@/app/lib/authOptions";
 import axios from "axios";
 
 export async function GET(req: Request) {
